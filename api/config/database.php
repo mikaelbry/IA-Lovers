a8d2bf8aae84e7c345675935d784a192dba5d0fc
mikaelbry <mikaelbr@protonmail.com>
2026-03-24 17:28:57 +0100

    Prueba de conexión a base de datos remota

--- api/config/database.php
+++ api/config/database.php
@@ -5,15 +5,25 @@ class Database {
     private $pdo;
 
     private function __construct() {
-        $this->pdo = new PDO(
-            "mysql:host=localhost;dbname=ialovers_v2;charset=utf8mb4",
-            "root",
-            "",
-            [
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-            ]
-        );
+        $host     = 'fdb1028.awardspace.net';
+        $db       = '4640876_ialovers';
+        $user     = '4640876_ialovers';
+        $pass     = 'F^Gr}raB9vnaqZ_Y';
+        $charset  = 'utf8mb4';
+
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+        $options = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+
+        try {
+            $this->pdo = new PDO($dsn, $user, $pass, $options);
+        } catch (PDOException $e) {
+            exit('Error de conexión BD: ' . $e->getMessage());
+        }
     }
 
     public static function getConnection() {
